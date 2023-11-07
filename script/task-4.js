ea6c956131d0f6a916237123a769bb064a59dbe0
Vitalii Lozovan <vitalulozovan@gmail.com>
2023-11-07 23:59:11 +0000

    change main code

--- script/task-4.js
+++ script/task-4.js
@@ -5,15 +5,6 @@
  як користувач позначив будь-які 3 чекбокси, 
  всі чекбокси робить неактивними.
  */
-// $(document).ready(function () {
-//     let checkboxes = $(".myCheckbox");
-//     checkboxes.change(function () {
-//         let checkedCount = checkboxes.filter(":checked").length;
-//         if (checkedCount >= 3) {
-//             checkboxes.not(":checked").attr("disabled", true);
-//         }
-//     });
-// });
 $(document).ready(function () {
     let checkboxes = $("input[type='checkbox']");
 
